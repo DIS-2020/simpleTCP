66340c482fa12071e840b58ec5950f377b8da7e3
mathiasrotheisen <mathiasrotheisen@gmail.com>
2020-08-30 21:45:30 +0200

    added comments

--- simpleTCP.js
+++ simpleTCP.js
@@ -1,11 +1,14 @@
+//Import net fra Node.js for at oprette socket
 const net = require("net")
 
+// Laver en socket og skriver "Hello" ved TCP connection
 const server = net.createServer(socket => {
     socket.write("Hello.")
+    //Ved modtagelse af data, skrives data i loggen
     socket.on("data", data => {
         console.log(data.toString())
     })
 })
 
-
+//Lytter ved port 8080 (HTTP)
 server.listen(8080)
